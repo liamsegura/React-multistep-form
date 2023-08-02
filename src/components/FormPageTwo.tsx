becfe4a98058b497d642f774b56f33781d81b790
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-08-02 13:10:36 +0100

    plan

--- src/components/FormPageTwo.tsx
+++ src/components/FormPageTwo.tsx
@@ -10,18 +10,32 @@ interface FormTwoProps {
   onChange: (name: string, value: string) => void;
   checked: boolean;
 }
+interface SelectableDivProps {
+  label: string;
+  selected: boolean;
+  onClick: () => void;
+}
+
+const SelectableDiv: React.FC<SelectableDivProps> = ({
+  label,
+  selected,
+  onClick,
+}) => {
+  return (
+    <div
+      className={`selectable-div ${selected ? "selected" : ""}`}
+      onClick={onClick}
+    >
+      {label}
+    </div>
+  );
+};
 const FormPageOne: React.FC<FormTwoProps> = ({
   formData,
   onNext,
   onPrevious,
   onChange,
-  checked,
 }) => {
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    onChange(name, value);
-  };
-
   const [checkBox, setCheckBox] = useState(false);
 
   const handleCheckBox = () => {
@@ -29,17 +43,53 @@ const FormPageOne: React.FC<FormTwoProps> = ({
     console.log(checkBox);
   };
 
+  const [selectedDivs, setSelectedDivs] = useState<number | null>(null);
+
+  const handleDivClick = (index: number) => {
+    setSelectedDivs(index);
+    onChange("plan", `Div ${index + 1}`);
+  };
+
   return (
     <div className="container">
       <h2>Page Two</h2>
-      <input
-        type="text"
-        name="email"
-        value={formData.plan}
-        onChange={handleInputChange}
-        placeholder="Enter your email"
-      />
-      {checkBox ? <h1>true</h1> : <h1>false</h1>}
+      {!checkBox ? (
+        <>
+          <SelectableDiv
+            label="Div 1"
+            selected={selectedDivs === 0}
+            onClick={() => handleDivClick(0)}
+          />
+          <SelectableDiv
+            label="Div 2"
+            selected={selectedDivs === 1}
+            onClick={() => handleDivClick(1)}
+          />
+          <SelectableDiv
+            label="Div 3"
+            selected={selectedDivs === 2}
+            onClick={() => handleDivClick(2)}
+          />
+        </>
+      ) : (
+        <>
+          <SelectableDiv
+            label="Div 4"
+            selected={selectedDivs === 3}
+            onClick={() => handleDivClick(3)}
+          />
+          <SelectableDiv
+            label="Div 5"
+            selected={selectedDivs === 4}
+            onClick={() => handleDivClick(4)}
+          />
+          <SelectableDiv
+            label="Div 6"
+            selected={selectedDivs === 5}
+            onClick={() => handleDivClick(5)}
+          />
+        </>
+      )}
       <label className="switch">
         <input type="checkbox" onChange={handleCheckBox} checked={checkBox} />
         <span className="slider round"></span>
